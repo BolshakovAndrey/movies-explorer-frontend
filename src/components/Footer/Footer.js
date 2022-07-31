10152415c9755f533efa7e11424923943b3593ba
Andrey Bolshakov <abolshakovy@gmail.com>
2022-07-31 22:41:22 +0300

    Feature(Style) Add Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,37 +1,45 @@
+import React from 'react';
 import {Link} from "react-router-dom";
+import './Footer.css';
 
 function Footer() {
+    const FOOTER_LINKS = [
+        {
+            title: 'Яндекс.Практикум',
+            link: 'https://practicum.yandex.com',
+        },
+        {
+            title: 'Github',
+            link: "https://github.com/BolshakovAndrey",
+        },
+        {
+            title: 'Telegram',
+            link: "https://t.me/Bolshakov_Andrey" ,
+        },
+    ];
+
     return(
-        <footer className="footer">
-            <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-            <div>
-                <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
-                <ul className="footer__links">
-                    <li>
-                        <Link
-                            className="footer__link"
-                            to={{ pathname: "https://practicum.yandex.ru/" }}
-                            target="_blank" >
-                            Яндекс.Практикум
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className="footer__link"
-                            to={{ pathname: "https://github.com/BolshakovAndrey" }}
-                            target="_blank" >
-                            Github
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className="footer__link"
-                            to={{ pathname: "https://t.me/Bolshakov_Andrey" }}
-                            target="_blank" >
-                            Telegram
-                        </Link>
-                    </li>
-                </ul>
+        <footer className="footer page__footer">
+            <div className="footer__container">
+                <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+                <div className="footer__content">
+                    <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+                    <ul className="footer__links">
+                        {FOOTER_LINKS.map((item, index) => (
+                            <li
+                                key={index}
+                            >
+                                <Link
+                                    className="footer__link"
+                                    to={{ pathname: item.link }}
+                                    target="_blank"
+                                >
+                                    {item.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </footer>
     );
